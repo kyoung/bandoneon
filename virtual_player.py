c24178b07ca9c4efde0829cd425c4c91615892bc
kyoung <mail@kyoung.ca>
2018-03-26 23:16:31 -0700

    relevant message types and properties

--- virtual_player.py
+++ virtual_player.py
@@ -8,19 +8,24 @@ import mido
 
 from bandoneon import button, bellows
 
+
 BELLOWS_FILE = bellows._VIRTUAL_BELLOWS_FILE
 BUTTON_FILE = button._VIRTUAL_BUTTONS_FILE
-
-
 CUMPARSITA = 'cumps.mid'
 
 
 def main():
     with mido.MidiFile(CUMPARSITA) as midi:
         for message in midi.play():
+            if message.type in ['control_change', 'program_change']:
+                continue
             # print out to the files
+            if message.type in ['note_on', 'note_off']:
+                note = message.note
+                velocity = message.velocity
+                time = message.time  # The time attribute of each message is the number of seconds since the last message or the start of the file.
+
             import pdb; pdb.set_trace()
-            pass
 
 
 if __name__ == '__main__':
